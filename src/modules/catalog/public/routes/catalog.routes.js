2866d9920618a3f23cd436b978d4a26a2ffa8c16
moy <169116592+moy2005@users.noreply.github.com>
2026-05-28 16:04:26 -0600

    Update catalog.routes.js

--- src/modules/catalog/public/routes/catalog.routes.js
+++ src/modules/catalog/public/routes/catalog.routes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import catalogController from '../controllers/catalog.controller.js';
-import { authMiddleware } from "../../../../core/middleware/auth.middleware.js";
-import { roleMiddleware } from "../../../../core/middleware/role.middleware.js";
+
 
 const router = express.Router();
 
